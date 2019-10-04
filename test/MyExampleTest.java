922663f76cbf51a64685554c0d62212eb6335e16
James Willenbring <jim.mn21@yahoo.com>
2019-10-04 08:20:30 -0500

    Create MyExampleTest.java

---
+++ test/MyExampleTest.java
@@ -0,0 +1,13 @@
+package io.github.jwillenbring.travis-example
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AddTest {
+
+@Test
+public void testSum() {
+  Sum mySum = new Sum();
+  int = mySum.sum(1,2);
+  assertEquals(3);
+  }
+}
